3463d15ed29d2b4e10de2f6a35291261b238577a
Nikhil Menghani <nikhil@menghani.com>
2021-07-06 18:42:01 -0400

    Fix Key Error while fetching environment variable

--- NikGapps/Git/Config.py
+++ NikGapps/Git/Config.py
@@ -7,7 +7,7 @@ if git_token_admin is None:
 owner = "nikgapps"
 repo = "config"
 
-display_response = os.environ['DISPLAY_RESPONSE']
+display_response = os.environ.get('DISPLAY_RESPONSE')
 if display_response is None:
     display_response = ""
 elif str(display_response).__eq__("1"):
