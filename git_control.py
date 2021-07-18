2381e440f207ea66b1d9c0ac543c678467c414d9
Nikhil Menghani <nikhil@menghani.com>
2021-07-18 15:12:15 -0400

    Close the pull request if no action is performed in 24 hours

--- git_control.py
+++ git_control.py
@@ -1,6 +1,13 @@
+from datetime import datetime, timedelta
+from math import floor
+
+from dateutil import parser as dateutil_parser
+import pytz
+
 from NikGapps.Git.Validate import Validate
 from NikGapps.Git.GitApi import GitApi
 from NikGapps.Git.PullRequest import PullRequest
+import NikGapps.Git.GitConfig as Config
 
 print("Checking if there is any existing workflow in progress")
 
@@ -53,9 +60,33 @@ for request in requests:
             last_commenter = comments[comments_len - 1]["user"]["login"]
             comment_body = comments[comments_len - 1]["body"]
             if str(last_commenter).__eq__("nikgapps") or str(last_commenter).__eq__("nikhilmenghani"):
-                print("Comment already made")
-                print(comment_body)
-                print("Moving on!")
+                updated_at = comments[comments_len - 1]["updated_at"]
+                try:
+                    d = dateutil_parser.parse(updated_at)
+                    print("Comment already made on: " + str(d))
+                    print()
+                    print(comment_body)
+                    print()
+                    datetime_utc = datetime.now(pytz.timezone('UTC'))
+                    date_diff = datetime_utc - d
+                    seconds = date_diff.seconds + (date_diff.days * 86400)
+                    hours = floor(seconds / 3600)
+                    sec = timedelta(seconds=seconds)
+                    d = datetime(1, 1, 1) + sec
+                    print("Last comment was %d:%d:%d:%d ago" % (d.day - 1, d.hour, d.minute, d.second))
+                    if hours >= Config.hours_before_close_pr:
+                        message = None
+                        if not str(comment_body).__contains__(f"Closing as no action was performed in last"):
+                            message = f"Closing as no action was performed in last {Config.hours_before_close_pr} hours"
+                        print("Closing the pull request")
+                        if GitApi.close_pull_request(pull_number=pr_number, message=message):
+                            print("Successfully Closed!")
+                        else:
+                            print("Failed to close the pull request")
+                    else:
+                        print("Moving on!")
+                except Exception as e:
+                    print(f"Exception occurred while parsing updated_at {updated_at} : " + str(e))
                 continue
         GitApi.comment_on_pull_request(pr_number, failure_reason)
     else:
