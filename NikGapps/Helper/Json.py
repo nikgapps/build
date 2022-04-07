d39f9f0ba64ae1a4e77be6026c1d73eac2ed0310
Nikhil Menghani <nikhil@menghani.com>
2022-04-07 11:21:00 -0400

    checkin json class

--- NikGapps/Helper/Json.py
+++ NikGapps/Helper/Json.py
@@ -4,18 +4,25 @@ import json
 
 class Json:
     @staticmethod
-    def read_from_url(url):
+    def read_from_url(url, beautify=False):
         response = urlopen(url)
         data_json = json.loads(response.read())
-        # print(data_json)
-        # print(json.dumps(data_json, indent=4, sort_keys=True))
         # print(data_json["manager"]["url"])
         # print(data_json[0]["url"])
         # print(len(data_json))
-        for i in range(0, len(data_json)):
-            print(data_json[i]["url"])
+
+        # for i in range(0, len(data_json)):
+        #     print(data_json[i]["url"])
         return data_json
+
+    @staticmethod
+    def print_from_url(url, beautify=False):
+        data_json = Json.read_from_url(url, beautify)
+        if beautify:
+            data_json = json.dumps(data_json, indent=4, sort_keys=True)
+        print(data_json["AdAway"]["version"])
 
 # https://api.github.com/repos/nikgapps/config/pulls/257/files
 # Json.read_from_url("https://mirror.codebucket.de/vanced/api/v1/latest.json")
-Json.read_from_url("https://api.github.com/repos/nikgapps/config/pulls")
+# Json.read_from_url("https://api.github.com/repos/nikgapps/config/pulls")
+Json.print_from_url("https://raw.githubusercontent.com/nikgapps/tracker/main/10_packages.json")
