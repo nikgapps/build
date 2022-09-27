9ed07f726616ac3acd10a0746895553b42b348be
Nikhil Menghani <nikhil@menghani.com>
2022-09-26 23:56:23 -0400

    Display a Failure message when building fails

--- NikGapps/Config/UserBuild/OnDemand.py
+++ NikGapps/Config/UserBuild/OnDemand.py
@@ -10,8 +10,7 @@ class OnDemand:
     def build_from_config_byte(config_name, config_in_byte, android_version):
         try:
             config_string = B64.b64d(config_in_byte)
-            OnDemand.build_from_config_string(config_name, config_string, android_version)
-            return True
+            return OnDemand.build_from_config_string(config_name, config_string, android_version)
         except Exception as e:
             print(e)
             return False
@@ -22,13 +21,14 @@ class OnDemand:
         config_dir.setup(override_dir=False)
         path = config_dir.write_user_config(config_string=config_string, android_version=android_version,
                                             config_name=config_name)
-        OnDemand.build_from_config_file(config_path=path, android_version=android_version)
+        return OnDemand.build_from_config_file(config_path=path, android_version=android_version)
 
     @staticmethod
     def build_from_config_file(config_path, android_version):
         config_obj = NikGappsConfig(config_path=config_path, use_zip_config=1)
+        result = False
         if config_obj.validate():
             # create a config based build
             Constants.update_android_version_dependencies()
-            Operations.build(config_obj, android_version)
-
+            result = Operations.build(config_obj, android_version)
+        return result
