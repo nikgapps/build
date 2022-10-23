e60122370334854e29d8f1ed66192642e7947541
Nikhil Menghani <nikhil@menghani.com>
2022-10-23 14:04:23 -0400

    We're not supposed to delete Build

--- cleanup.py
+++ cleanup.py
@@ -3,7 +3,7 @@ from NikGapps.Helper import FileOp, Constants
 
 start_time = Constants.start_of_function()
 
-folders_to_delete = ['10', '11', '12', '12.1', '13', 'Build', 'Releases', 'canary-release', 'release',
+folders_to_delete = ['10', '11', '12', '12.1', '13', 'Releases', 'canary-release', 'release',
                      'nikgapps.github.io', 'TempPackages', 'overlays_SL', 'overlays_T']
 for folder in folders_to_delete:
     if os.path.exists(Constants.pwd + Constants.dir_sep + folder):
