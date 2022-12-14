64c369fc48ba22ab14f275857dad99d1726b3842
Nikhil Menghani <nikhil@menghani.com>
2022-12-14 09:56:59 -0500

    Avoid repetition of OEM processing

--- NikGapps/OEM/Sync.py
+++ NikGapps/OEM/Sync.py
@@ -8,9 +8,17 @@ class Sync:
 
     def do(self, android_version, list_of_supported_oems=None):
         list_of_appsets = self.appsets
+        supported_oems, controller_appsets = Operations.get_oems_from_controller(android_version,
+                                                                                 self.tracker_repo)
         if list_of_supported_oems is None:
-            list_of_supported_oems, list_of_appsets = Operations.get_oems_from_controller(android_version,
-                                                                                          self.tracker_repo)
+            list_of_supported_oems = supported_oems
+        else:
+            for oem in supported_oems:
+                if oem not in list_of_supported_oems:
+                    list_of_supported_oems.append(oem)
+        for appset in controller_appsets:
+            if appset not in list_of_appsets:
+                list_of_appsets.append(appset)
         for oem in list_of_supported_oems:
             Operations.sync_tracker(android_version=android_version, oem=oem, appsets=list_of_appsets,
                                     tracker_repo=self.tracker_repo)
