e83fe9fc90aff5c71760472737ef037901fc1c37
Nikhil Menghani <nikhil@menghani.com>
2023-02-12 21:47:12 -0500

    Remove comment that is no longer necessary

--- NikGapps/Compression/Tar.py
+++ NikGapps/Compression/Tar.py
@@ -10,12 +10,6 @@ class Tar:
         self.tarfile = str(file_name) if str(file_name).endswith("tar.xz") else str(file_name) + ".tar.xz"
         self.tar = tarfile.open(self.tarfile, "w:xz")
 
-    # def compress(self):
-    #     start_time = C.start_of_function()
-    #     self.tar.add(self.folder, arcname=self.folder.stem)
-    #     print(f"py tar filesize: {round(Path(self.tarfile).stat().st_size / (1024 * 1024), 2)} MB")
-    #     C.end_of_function(start_time, "py tar filesize")
-
     def add_file(self, file_to_add, zippath):
         self.tar.add(file_to_add, arcname=zippath)
 
