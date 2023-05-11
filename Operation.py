358021f33780359a6b6717d34cc7d2c7c080cc96
Nikhil Menghani <nikhil@menghani.com>
2023-05-11 19:45:47 -0400

    Bring in Overlays

--- Operation.py
+++ Operation.py
@@ -65,6 +65,7 @@ class Operation:
         else:
             print("Git Check is disabled")
         for android_version in android_versions:
+            android_version_str = str(android_version)
             upload = Upload(android_version=android_version, upload_files=Config.UPLOAD_FILES,
                             release_type=Config.RELEASE_TYPE)
             Config.TARGET_ANDROID_VERSION = android_version
@@ -74,21 +75,22 @@ class Operation:
                 release_datetime = None
                 if release_repo is not None:
                     release_datetime = release_repo.get_latest_commit_date(branch="master",
-                                                                           filter_key=str(android_version))
-                    print(f"Last Release ({str(android_version)}): " + str(release_datetime))
-                apk_source_datetime = GitOperations.get_last_commit_date(android_version=str(android_version),
+                                                                           filter_key=android_version_str)
+                    print(f"Last Release ({android_version_str}): " + str(release_datetime))
+                apk_source_datetime = GitOperations.get_last_commit_date(android_version=android_version_str,
                                                                          branch="main" if Config.RELEASE_TYPE.__eq__(
                                                                              "stable") else "canary")
                 if apk_source_datetime is not None:
-                    print("Last Apk Repo (" + str(Config.TARGET_ANDROID_VERSION) + ") Commit: " + str(
-                        apk_source_datetime))
+                    print("Last Apk Repo (" + android_version_str + ") Commit: " + str(apk_source_datetime))
+                    if float(android_version) >= 12.1:
+                        GitOperations.clone_overlay_repo(android_version, fresh_clone=False, branch="master")
                 else:
-                    print(Statics.pwd + Statics.dir_sep + str(android_version) + " doesn't exist!")
+                    print(Statics.pwd + Statics.dir_sep + android_version_str + " doesn't exist!")
                 new_release = self.is_new_release(source_last_commit_datetime, apk_source_datetime, release_datetime)
             if Config.OVERRIDE_RELEASE or new_release:
                 Release.zip(package_list, android_version, sign_zip, send_zip_device, fresh_build, telegram, upload)
                 if release_repo is not None and git_check:
-                    release_repo.git_push(str(android_version) + ": " + str(commit_message))
+                    release_repo.git_push(android_version_str + ": " + str(commit_message))
             if Config.UPLOAD_FILES:
                 config = NikGappsConfig(android_version=android_version)
                 ConfigOperations.upload_nikgapps_config(config, android_version, upload)
